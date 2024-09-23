8e0c8620782613cbff1a394162a1f1342e16c7aa
youngzhenhao <2499342961@qq.com>
2024-09-23 08:38:11 +0800

    update

--- http_demo/main.go
+++ http_demo/main.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"fmt"
-	"go-web-advance/utils"
+	"html"
 	"io"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"net/url"
 )
 
 func main() {
+	demo2()
+}
+
+func demo1() {
 	resp, err := http.Get("http://www.bing.com")
 	if err != nil {
 		fmt.Println(err)
@@ -24,5 +31,40 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	fmt.Println(utils.ValueJsonString(body))
+	fmt.Println(string(body))
+}
+
+func demo2() {
+	apiUrl := "http://127.0.0.1:9090/get"
+	// URL param
+	data := url.Values{}
+	data.Set("name", "alice")
+	data.Set("age", "18")
+	u, err := url.ParseRequestURI(apiUrl)
+	if err != nil {
+		fmt.Printf("parse url requestUrl failed, err:%v\n", err)
+	}
+	u.RawQuery = data.Encode() // URL encode
+	fmt.Println(u.String())
+	resp, err := http.Get(u.String())
+	if err != nil {
+		fmt.Printf("post failed, err:%v\n", err)
+		return
+	}
+	defer resp.Body.Close()
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("get resp failed, err:%v\n", err)
+		return
+	}
+	fmt.Println(string(b))
+}
+
+func demo3() {
+	//http.Handle("/foo", fooHandler)
+	http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
 }
